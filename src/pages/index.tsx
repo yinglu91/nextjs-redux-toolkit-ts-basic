61794c61ad44607b2153c27b921e746831684291
ylu <ylu@ert.com>
2021-08-18 14:40:57 -0400

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,40 @@
+import { useState, FC } from "react"
+import { useAppDispatch, useAppSelector } from "../app/hooks"
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  selectCount,
+} from "../features/counter/counterSlice"
+
+const IndexPage: FC = () => {
+  const dispatch = useAppDispatch()
+  const count = useAppSelector(selectCount)
+
+  const [amount, setAmount] = useState(0)
+
+  return (
+    <>
+      <h1>Welcome to the greatest app in the world!</h1>
+      <h2>The current number is {count}</h2>
+
+      <div>
+        <input
+          value={amount}
+          onChange={(e) => setAmount(Number(e.target.value))}
+          type='number'
+        />
+        <button onClick={() => dispatch(incrementByAmount(Number(amount)))}>
+          Increment by amount
+        </button>
+      </div>
+
+      <div>
+        <button onClick={() => dispatch(decrement())}>Decrement by 1</button>
+        <button onClick={() => dispatch(increment())}>Increment by 1</button>
+      </div>
+    </>
+  )
+}
+
+export default IndexPage
